142fe82fbe5bf0858d8ba58c9cae6be3688b9023
jotabraga <jpmbraga.feg@gmail.com>
2024-05-08 13:46:07 -0300

    feat: add style to main page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,9 +2,11 @@ import { ReactElement } from "react";
 
 function Header(): ReactElement {
   return (
-    <div>
-      <h2 className="text-2xl font-medium">Seja bem-vindo</h2>
-      <p className="font-extrabold text-green-300">
+    <div className="flex flex-col gap-8">
+      <h2 className="font-openSans text-5xl text-green-950 font-bold">
+        Seja bem-vindo
+      </h2>
+      <p className="font-openSans text-green-400 text-3xl">
         Envie seu arquivo .csv para realizar as cobranças{" "}
       </p>
     </div>
--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -3,7 +3,8 @@ import { Outlet, useOutletContext } from "react-router-dom";
 import * as React from "react";
 import { FileUploader } from "./file-uploader";
 import { Header } from "./header";
-import { Table, TableHeader } from "./table";
+import { DataTable } from "./data-table";
+import { columns } from "./colums";
 
 type ContextType = { user: { name: string } | null };
 
@@ -14,13 +15,24 @@ export function Layout(): ReactElement {
     return useOutletContext<ContextType>();
   }
 
+  const mockedData = [{ name: "cobranca.csv", date: "08/05/2024" }];
+
   return (
     <>
       <main className="p-6 flex flex-col gap-8 justify-center items-center h-screen">
+        <a href="./" data-framer-page-link-current="true">
+          <div data-framer-background-image-wrapper="true">
+            <img
+              decoding="async"
+              src="https://framerusercontent.com/images/f0btmN2GtVDhwuoOUM5xAjorM.png"
+              alt=""
+            />
+          </div>
+        </a>
         <Header />
+
         <FileUploader />
-        <Table />
-        <TableHeader />
+        <DataTable columns={columns} data={mockedData} />
         <Outlet context={{ user } satisfies ContextType} />
       </main>
     </>
